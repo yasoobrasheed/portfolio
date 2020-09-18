dda82b5bebaabb028125faec8912d9f885af1a65
Yasoob Rasheed <yasoobr@Yasoobs-Air.hsd1.il.comcast.net>
2020-09-18 14:31:16 -0500

    some stylistic changes made to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import './Home.css'
-import instagram from '../../assets/instagram.png'
-import facebook from '../../assets/facebook.png'
-import linkedin from '../../assets/linkedin.png'
-import github from '../../assets/github.png'
-import soundcloud from '../../assets/soundcloud.png'
 
 
 export const Home = () => {
@@ -13,40 +8,7 @@ export const Home = () => {
     <div>
       <Jumbotron fluid id="jumbo">
       </Jumbotron>
-
-      <div className="media-links">
-        <a href="https://github.com/yasoobrasheed" target="_blank" rel="noopener noreferrer">
-        <button id="socialbutton">
-          <img className="logo" alt="gh" src={github}/>
-        </button>
-        </a>
-
-        <a href="https://www.linkedin.com/in/yasoobrasheed/" target="_blank" rel="noopener noreferrer">
-        <button id="socialbutton">
-          <img className="logo" alt="li" src={linkedin}/>
-        </button>
-        </a>
-
-        <a href="https://www.instagram.com/yasoobr/" target="_blank" rel="noopener noreferrer">
-        <button id="socialbutton">
-          <img className="logo" alt="insta" src={instagram}/>
-        </button>
-        </a>
-
-        <a href="https://www.facebook.com/yasoob.rasheed" target="_blank" rel="noopener noreferrer">
-        <button id="socialbutton">
-          <img className="logo" alt ="fb" src={facebook}/>
-        </button>
-        </a>
-
-        <a href="https://soundcloud.com/yasooob" target="_blank" rel="noopener noreferrer">
-        <button id="socialbutton">
-          <img className="logo" alt="dc" src={soundcloud}/>
-        </button>
-        </a>
-      </div>
-      <p id="cheekytext">Ah yes.. I see I've got your attention. Try checking out some of my work on the top left.</p>
-
+      <p id="cheekytext">What are you doing down here?!</p>
     </div>
   )
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import logo from '../assets/logo.png'
+import logo from '../assets/logo3.png'
+import trans from '../assets/transparent.png'
+import instagram from '../assets/instagram.png'
+import facebook from '../assets/facebook.png'
+import linkedin from '../assets/linkedin.png'
+import github from '../assets/github.png'
+import soundcloud from '../assets/soundcloud.png'
 import "./NavBar.css"
 
 export const NavBar = () => (
-    <Navbar expand="lg">
+    <Navbar expand="lg" fixed="top" variant="dark">
 
       <Navbar.Brand href="/">
         <a>
           <img href="/" id="facelogo" alt="face" src={logo} width="40" height="40"/>
+          <img id="block" alt="blank" src={trans} width="10" height="40"/>
           Yasoob Rasheed
         </a>
       </Navbar.Brand>
@@ -19,14 +26,34 @@ export const NavBar = () => (
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Item>
-            <Nav.Link href="/projects"> Projects </Nav.Link>
+            <Nav.Link id="navitem" href="/projects"> Projects </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/tutoring"> Tutoring </Nav.Link>
+            <Nav.Link id="navitem" href="/tutoring"> Tutoring </Nav.Link>
           </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/realestate"> Real Estate </Nav.Link>
-          </Nav.Item>
+        </Nav>
+        <Nav className="ml-auto">
+          <div id="media-links">
+            <a href="https://github.com/yasoobrasheed" target="_blank" rel="noopener noreferrer">
+              <img className="logo" alt="gh" src={github}/>
+            </a>
+            <img id="block" alt="blank" src={trans} width="7.5" height="40"/>
+            <a href="https://www.linkedin.com/in/yasoobrasheed/" target="_blank" rel="noopener noreferrer">
+              <img className="logo" alt="li" src={linkedin}/>
+            </a>
+            <img id="block" alt="blank" src={trans} width="7.5" height="40"/>
+            <a href="https://www.instagram.com/yasoobr/" target="_blank" rel="noopener noreferrer">
+              <img className="logo" alt="insta" src={instagram}/>
+            </a>
+            <img id="block" alt="blank" src={trans} width="7.5" height="40"/>
+            <a href="https://www.facebook.com/yasoob.rasheed" target="_blank" rel="noopener noreferrer">
+              <img className="logo" alt ="fb" src={facebook}/>
+            </a>
+            <img id="block" alt="blank" src={trans} width="7.5" height="40"/>
+            <a href="https://soundcloud.com/yasooob" target="_blank" rel="noopener noreferrer">
+              <img id="link" className="logo" alt="dc" src={soundcloud}/>
+            </a>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
