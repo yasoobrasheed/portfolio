645bc8e8572950dae19ed40b97fb4542e6f03155
Yasoob Rasheed <yasoobr@Yasoobs-Air.hsd1.il.comcast.net>
2020-09-19 01:55:58 -0500

    fe2 & might be done for the night

--- src/Components/Projects/ProjectCover.js
+++ src/Components/Projects/ProjectCover.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ProjectCover = (props) => {
   return (
-    <div className="hvr-grow">
+    <div className="hvr-grow" id="coverdiv">
       <button id="projectbutton">
         <h3 id="header">{props.projtitle}</h3>
         <img id="imgbanner" alt="projimage" src={props.imgsrc} />
