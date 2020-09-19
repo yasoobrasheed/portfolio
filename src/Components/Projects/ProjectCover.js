015fcf4b9067f0ec22107d320aa212ec7b3aff4e
Yasoob Rasheed <yasoobr@Yasoobs-Air.hsd1.il.comcast.net>
2020-09-19 01:44:05 -0500

    see if button is the issue

--- src/Components/Projects/ProjectCover.js
+++ src/Components/Projects/ProjectCover.js
@@ -3,10 +3,10 @@ import React from 'react'
 export const ProjectCover = (props) => {
   return (
     <div>
-      <button className="hvr-grow" id="projectbutton">
-      <h3 id="header">{props.projtitle}</h3>
-      <img id="imgbanner" alt="projimage" src={props.imgsrc} />
-      <p id="covertext">{props.projtext}</p>
+      <button id="projectbutton">
+        <h3 className="hvr-grow" id="header">{props.projtitle}</h3>
+        <img id="imgbanner" alt="projimage" src={props.imgsrc} />
+        <p id="covertext">{props.projtext}</p>
       </button>
     </div>
   )
--- src/Components/Tutoring/Tutoring.js
+++ src/Components/Tutoring/Tutoring.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { NavBar } from '../NavBar'
 
 export const Tutoring = () => {
   return (
     <div>
+      <NavBar navType="project" variant="light"/>
     </div>
   )
 }
