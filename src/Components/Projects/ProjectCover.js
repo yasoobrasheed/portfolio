4c60dea87fdb71842a2b6cb7c2c21dda9cce01f4
Yasoob Rasheed <yasoobr@Yasoobs-Air.hsd1.il.comcast.net>
2020-09-19 01:40:50 -0500

    trying to fix small rendering issue with project covers

--- src/Components/Projects/ProjectCover.js
+++ src/Components/Projects/ProjectCover.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ProjectCover = (props) => {
   return (
     <div>
-      <button className="hvr-bounce-to-right" id="projectbutton">
+      <button className="hvr-grow" id="projectbutton">
       <h3 id="header">{props.projtitle}</h3>
       <img id="imgbanner" alt="projimage" src={props.imgsrc} />
       <p id="covertext">{props.projtext}</p>
