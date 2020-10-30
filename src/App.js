e69b5de1584da82ee8a3323bc9c32de53aff375a
Yasoob Rasheed <yasoobr@Yasoobs-Air.hsd1.il.comcast.net>
2020-10-29 23:52:00 -0500

    now build will work

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { Home } from './Components/Home/Home'
 import { Projects } from './Components/Projects/Projects'
-import { Poetry } from './Components/Poetry/Poetry'
 import { About } from './Components/About/About'
 import { NoMatch } from './Components/NoMatch'
 
