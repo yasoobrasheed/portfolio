75197e33ff86b06e947a6d8e6a94820405ea36d7
Yasoob Rasheed <yasoob.f.rasheed@gmail.com>
2020-12-08 15:46:25 -0600

    vacuum clean v1
    Removed poems & altered about description

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,13 +7,13 @@ import { HeadView } from './HeadView'
 
 const header = "Welcome to my site!"   
 
-const text1 = "I’m Yasoob, a recent Computer Science & Creative Writing Grad from the University of Chicago. In my career so far, I’ve been a co-founder, a software engineer, a research fellow, and a management consultant."
+const text1 = "I’m Yasoob, a recent Computer Science & Creative Writing graduate from The University of Chicago. In my career so far, I’ve been a co-founder, a software engineer, a research fellow, and a management consultant."
 
 const text2 = "Currently, I am a full stack developer at a rapidly growing fintech startup called CardX. On a daily basis I use Node, React, Vue, and AWS. During our sprints I am building our Git repos, APIs, servers, databases, and testing."
 
 const text3 = "I also love teaching. I run a CS and Design Thinking course for 25 students in the West Bank as a member of Stanford’s Code for Palestine. I am a technology literacy mentor for refugee students from Syria and the Congo. I am also a private tutor for over 10 of my own students."
 
-const text4 = "Please check out a few of my projects in the project tab, and it is my pleasure to present my poetry portfolio in the poetry tab. Thanks for being here and please don't hesitate to connect with me through LinkedIn."
+const text4 = "Please check out my projects in the project tab, and it is my pleasure to present my poetry portfolio in the poetry tab. Thanks for being here and please don't hesitate to connect with me through LinkedIn."
 
 
 export const About = () => {
--- src/Components/Poetry/Poetry.js
+++ src/Components/Poetry/Poetry.js
@@ -3,19 +3,16 @@ import './Poetry.css'
 import { NavBar } from '../NavBar'
 
 import page0 from './assets/0.jpg'
-import page1 from './assets/1.jpg'
 import page2 from './assets/2.jpg'
 import page3 from './assets/3.jpg'
 import page4 from './assets/4.jpg'
 import page5 from './assets/5.jpg'
-import page6 from './assets/6.jpg'
-import page8 from './assets/8.jpg'
 import page9 from './assets/9.jpg'
 import page10 from './assets/10.jpg'
 
 export class Poetry extends React.Component {
     render() {
-        var items = [page0, page1, page2, page3, page4, page5, page6, page8, page9, page10]
+        var items = [page0, page2, page3, page4, page5, page9, page10]
         return (
             <div>
                 <NavBar navType="project" variant="light"/>
