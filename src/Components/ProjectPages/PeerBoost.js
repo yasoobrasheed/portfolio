a8baedf800a61a9f70076ecc77b05ad36d323dbc
Yasoob Rasheed <yasoob.f.rasheed@gmail.com>
2021-05-08 19:52:38 -0500

    :lipstick: copy changes for website post 6 months

--- src/Components/ProjectPages/PeerBoost.js
+++ src/Components/ProjectPages/PeerBoost.js
@@ -6,13 +6,13 @@ import { TextView } from './Views/TextView';
 import { HeaderView } from './Views/HeaderView';
 
 export const PeerBoost = () => {
-  const text = "My first venture. A UChicago Booth New Venture Challenge finalist that connects clients with microinfluencers bent on selling their products."
+  const text = "A UChicago Booth New Venture Challenge finalist that connects companies with microinfluencers."
   return (
     <div>
       <NavBar navType="project" variant="light"/>
       <SideBar />
       <HeaderView text={text}/>
-      <TextView text={"Coming Soon! Check out the video below in the meantime."} />
+      <TextView text={"A fun little venture idea a friend and I had back in 2018. Below, you can see the pitch we made for the competition."} />
       <VideoView vidLink="https://www.youtube.com/embed/Q1e2BfABYPU"/>
     </div>
   )
